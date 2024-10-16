8122b660a1ba60cf1c77517984e7e2d68b492305
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-10-16 19:01:14 +0300

    a new try

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -13,7 +13,7 @@ export default function Error({}: GlobalErrorProps) {
     <html>
       <body>
         <div>
-          <p>Something globally went wrong</p>
+          <p>Something globally went wrong!</p>
         </div>
       </body>
     </html>
